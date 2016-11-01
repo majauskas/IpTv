970f8ef71de64bc347457685f67a45867ffc5623
minde-ags <m.ajauskas@hotmail.it>
2016-11-01 19:39:50 +0100

    t

--- node-js/server.js
+++ node-js/server.js
@@ -50,6 +50,42 @@ this.start = function() {
 //			callback({});		
 		});
 	  
+		  console.log("OK nodeCEC");
+		  var cec = new NodeCEC();
+
+		//start cec connection
+		cec.start();
+
+		cec.on('ready', function(data) {
+		   console.log("ready...");
+		});
+
+		cec.on('status', function(data) {
+		  console.log("[" + data.id + "] changed from " + data.from + " to " + data.to); 
+		});
+
+		cec.on('key', function(data) {
+		   console.log(data.name);
+		   io.sockets.emit("SOCKET-REMOTE-CONTROLL", {key:data.name});
+		   
+		});
+
+		cec.on('close', function(code) {
+		   process.exit(0);
+		});
+
+		cec.on('error', function(data) {
+		   console.log('---------------- ERROR ------------------');
+		   console.log(data);
+		   console.log('-----------------------------------------');
+		});
+
+		var stdin = process.openStdin();
+		stdin.on('data', function(chunk) { 
+		   cec.send(chunk);
+		});
+		
+		
 		
 		
 	  
@@ -76,40 +112,7 @@ this.start = function() {
   
   
   
-  console.log("OK nodeCEC");
-  var cec = new NodeCEC();
 
-//start cec connection
-cec.start();
-
-cec.on('ready', function(data) {
-   console.log("ready...");
-});
-
-cec.on('status', function(data) {
-  console.log("[" + data.id + "] changed from " + data.from + " to " + data.to); 
-});
-
-cec.on('key', function(data) {
-   console.log(data.name);
-   io.sockets.emit("SOCKET-REMOTE-CONTROLL", {key:data.name});
-   
-});
-
-cec.on('close', function(code) {
-   process.exit(0);
-});
-
-cec.on('error', function(data) {
-   console.log('---------------- ERROR ------------------');
-   console.log(data);
-   console.log('-----------------------------------------');
-});
-
-var stdin = process.openStdin();
-stdin.on('data', function(chunk) { 
-   cec.send(chunk);
-});
   
   
   
